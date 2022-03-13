0f447737aa8e59132cf5aa76b03b8012f01a727d
shyam <shyam20998@gmail.com>
2022-03-13 07:43:32 +0530

    optimized sidebar for small screens

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './Sidebar.scss'
+import { useMediaQuery } from '../../misc/custom-hooks'
 import cartegoryData from '../../server/categories/index.get.json'
 import { NavLink, useParams } from 'react-router-dom'
+import { FaAngleDown } from 'react-icons/fa'
 
 const Sidebar = () => {
+    const [selectedCategory, setSelectedCategory] = useState('')
+    const [displaySidebar, setDisplaySidebar] = useState(false)
     const catId = useParams()
-    
-    return (
+
+    const isDesktop = useMediaQuery('(min-width: 600px)')
+
+    useEffect(() => {
+        console.log(cartegoryData)
+        const currentCategory = cartegoryData.filter(p => p.id === catId.id)
+        setSelectedCategory(currentCategory[0].name)
+    }, [])
+
+    const clickHandler = (name) => {
+        setSelectedCategory(name)
+        setDisplaySidebar(false)
+    }
+
+    const sidebar = (
         <ul className="sidebar">
             {
                 cartegoryData.map((category, i) => {
                     return (
-                        <NavLink key={i} to={`/products/${category.id}`}>
+                        <NavLink key={i} to={`/products/${category.id}`} onClick={() => clickHandler(category.name)}>
                             <li className={category.id === catId.id ? 'sidebar__item active' : 'sidebar__item'}>
                                 {category.name}    
                             </li>
@@ -21,6 +38,24 @@ const Sidebar = () => {
                 })
             }
         </ul>
+    )
+    
+    return (
+        <>
+            { 
+                isDesktop ?
+                {...sidebar} :
+                <div>
+                    <div 
+                        className="sidebar-mobile" 
+                        onClick={() => setDisplaySidebar(!displaySidebar)}>
+                        <span className="sidebar-mobile__text">{selectedCategory}</span>
+                        <span className="sidebar-mobile__icon"><FaAngleDown/></span>
+                    </div>
+                    {displaySidebar && sidebar}
+                </div>
+            }
+        </>
     )
 }
 
